9940f983335762149722ef32543154ac67931422
Peter Cardena <pa.cardena@gmail.com>
2021-04-19 11:10:49 +0200

    Exercise 2 & 3 done

--- basic_exercises.php
+++ basic_exercises.php
@@ -25,7 +25,23 @@ echo "<h1>$myName</h1>";
 
 ?>
 
+<?php
 
+$name = "Peter";
+$age = "30";
+$jobTitle = "Sales Consultant";
+
+echo "Hi, my name is $name, I am $age, and I work as a $jobTitle."
+
+?>
+
+<?php
+
+$names = array('Mark', 'John', 'Georg', 'Lisa');
+
+echo "The third player in the team is " . $names[2];
+
+?>
     
 </body>
 </html>
