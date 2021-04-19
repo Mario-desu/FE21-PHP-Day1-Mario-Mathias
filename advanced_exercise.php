05a5f7dd5849094ee8693dcaf96e17e877a1478e
Peter Cardena <pa.cardena@gmail.com>
2021-04-19 11:33:36 +0200

    Array added

---
+++ advanced_exercise.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $dragonBall = array(
+        "Goku" => 1,
+        "Vegeta" => 2,
+        "Piccolo" => 3
+    );
+    echo "Power ranking of Goku is " . $dragonBall['Goku'] . "<br/>";
+    echo "Power ranking of Vegeta is " . $dragonBall['Vegeta'] . "<br/>";
+    echo "Power ranking of Piccolo is " . $dragonBall['Piccolo'] . "<br/>";
+    
+    $superMario = array(
+        "Mario" =>
+    )
+    ?>
+</body>
+</html>
